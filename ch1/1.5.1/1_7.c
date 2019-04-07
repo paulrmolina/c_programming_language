0eeae84e228f95d26a56a26bac77cbed441c1b8b
Paul Molina <paulrmolina@gmail.com>
2019-04-06 22:35:25 -0400

    Completed exercise 1-7

---
+++ ch1/1.5.1/1_7.c
@@ -0,0 +1,16 @@
+// Author:  Paul Molina
+// Date:    April 2019
+//
+// Description:
+// This was written in order to solve K&R's exercise 1-7.
+// It simply prints the value of EOF
+
+#include <stdio.h>
+
+int main()
+{
+  printf("The character value of EOF is: %c\n", EOF);
+  printf("The numeric value of EOF is: %d\n", EOF);
+
+  return 0;
+}
